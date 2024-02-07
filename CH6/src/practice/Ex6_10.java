abba05205c372ccc1554910d627772cf9f94804e
ten4roid <thdckdtjr8@gmail.com>
2024-02-07 14:22:09 +0900

    Ex6_10 update

--- CH6/src/practice/Ex6_10.java
+++ CH6/src/practice/Ex6_10.java
@@ -4,9 +4,9 @@ public class Ex6_10 {
 	public static void main(String[] args) {
 		MyMath3 mm = new MyMath3();
 		System.out.println("mm.add(3, 3) 결과 : " + mm.add(3, 3));
-		System.out.println("mm.add(3, 3) 결과 : " + mm.add(3L, 3));
-		System.out.println("mm.add(3, 3) 결과 : " + mm.add(3, 3L));
-		System.out.println("mm.add(3, 3) 결과 : " + mm.add(3L, 3L));
+		System.out.println("mm.add(3L, 3) 결과 : " + mm.add(3L, 3));
+		System.out.println("mm.add(3, 3L) 결과 : " + mm.add(3, 3L));
+		System.out.println("mm.add(3L, 3L) 결과 : " + mm.add(3L, 3L));
 		
 		int[] a = {100, 200, 300};
 		System.out.println("mm.add(a) 결과 : " + mm.add(a));
