95572c9bca37172a58c9c2d2389d098b1b2623e3
ten4roid <thdckdtjr8@gmail.com>
2024-02-07 23:49:02 +0900

    CHAPTER6 UPDATE

--- CH6/src/practice/Ex6_11.java
+++ CH6/src/practice/Ex6_11.java
@@ -2,20 +2,22 @@ package practice;
 
 class Data_1 {
 	int value;
+	Data_1(){} // 기본 생성자를 추가
 }
 
 class Data_2 {
 	int value;
 
+	Data_2() {} // 기본 생성자
+	
 	Data_2(int x) { // 매개변수가 있는 생성자
 		value = x;
 	}
-	
 }
 
 public class Ex6_11 {
 	public static void main(String[] args) {
 		Data_1 d1 = new Data_1();
-		Data_2 d2 = new Data_2(1); // compile error 발생
+		Data_2 d2 = new Data_2(); // compile error 발생
 	}
 }
